e4c3d004d0e9d26efcbe1bab602885931a4ff783
tvs_senthil <senthilkumarimuthu@gmail.com>
2024-01-23 20:27:53 +0530

    walmart sales prediction is added

---
+++ test.py
@@ -0,0 +1,13 @@
+lis1 = [1,2,3,4,3, 3, 2]
+x = 2
+result = []
+for i in lis1:
+    if i != x:
+          result.append(i)
+print(result)
+
+
+import base64
+
+out = base64.decodebytes("[B@1dd1d58")
+print(out.decode("ascii") )
