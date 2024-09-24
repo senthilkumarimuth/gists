c7af920b02634ff76758eef37cd5e26ca47e2096
tvs_senthil <senthilkumarimuthu@gmail.com>
2024-09-24 15:35:36 +0530

    vscode added to gitignore

--- test.py
+++ test.py
@@ -1,13 +1,9 @@
-lis1 = [1,2,3,4,3, 3, 2]
-x = 2
-result = []
-for i in lis1:
-    if i != x:
-          result.append(i)
-print(result)
+"""
 
+Write a python program which transform a string to small or capital letter based on n=2 0r 3 or 4
+for example input = "data Science" and n=2
+output = "DaTa ScIeNcE"
 
-import base64
+"""
 
-out = base64.decodebytes("[B@1dd1d58")
-print(out.decode("ascii") )
+def
