d56e1bc63cfe42cd7f6d10f760a0320d709f1527
tvs_senthil <senthilkumarimuthu@gmail.com>
2024-10-09 11:41:18 +0530

    prompt caching

--- test.py
+++ test.py
@@ -1,9 +1,6 @@
-"""
+import pandas as pd
 
-Write a python program which transform a string to small or capital letter based on n=2 0r 3 or 4
-for example input = "data Science" and n=2
-output = "DaTa ScIeNcE"
-
-"""
-
-def
+df = pd. read_excel(r"C:\\Users\\senthil.marimuthu\\Downloads\\CIM cases From Feb 24 to mar 24.xlsx")
+df =  df[df['assigned']=='senthil']
+print(len(df))
+print(df['Sim'].tolist())
