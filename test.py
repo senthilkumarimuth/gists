45f7f2e22e58d7bc0df4f3b6b48d017e1cbb53ce
senthilkumarimuth <senthilkumarimuthu@gmail.com>
2026-01-01 08:33:32 +0530

    Refactor test.py and add quantization helper functions: Remove CSV reading and profiling report generation from test.py, replacing it with a nested loop over a predefined list. Introduce a new helper.py file containing functions for plotting quantization errors and visualizing tensors, enhancing the quantization analysis capabilities. Additionally, add a new quantization_learning.ipynb notebook that provides an in-depth explanation of quantization concepts, methods, and practical implementations.

--- test.py
+++ test.py
@@ -1,18 +1,4 @@
-import os
-import glob
-import pandas as pd
-from ydata_profiling import ProfileReport
+given_list = ['ate', 'tea', 'eat', 'eta', 'tae', 'eta', 'bat','tab']
 
-
-
-# Read the CSV file into a DataFrame
-df = pd.read_csv(r"C:\Users\senthil.marimuthu\Downloads\derating_eda.csv")
-
-# Generate the pandas profiling report
-profile = ProfileReport(df, title="Pandas Profiling Report", explorative=True)
-
-# Save the report to an HTML file in the Downloads folder
-report_path = os.path.join(downloads_path, "pandas_profiling_report.html")
-profile.to_file(report_path)
-
-print(f"Profiling report saved to: {report_path}")
+for i in given_list:
+    for j in given_list:
